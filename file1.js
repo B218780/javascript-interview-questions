4b3f9d9e5c471dfbf37857050746890954a4b54c
bhikam <mlq41204167@gmail.com>
2023-09-27 23:54:21 +0530

    javascript interview questions

---
+++ file1.js
@@ -0,0 +1,19 @@
+// var 1firstName = "bhikam";
+// console.log(1firstName);
+
+// var firstName1 = "bhikam";
+// console.log(firstName1);
+
+// var first1Name = "bhikam";
+// console.log(first1Name);
+
+// var _firstName = "bhikam";
+// console.log(_firstName);
+
+// var $firstName = "bhikam";
+// console.log($firstName);
+
+var firstName& = "bhikam";
+console.log(firstName&);
+
+// var $firstName = "bhikam"
