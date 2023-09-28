e7eaacd03cb537a7f0e802233eb4e5700f897a30
bhikam <mlq41204167@gmail.com>
2023-09-28 14:05:34 +0530

    add more data of js

--- file1.js
+++ file1.js
@@ -13,7 +13,172 @@
 // var $firstName = "bhikam";
 // console.log($firstName);
 
-var firstName& = "bhikam";
-console.log(firstName&);
+// var firstName& = "bhikam";
+// console.log(firstName&);
 
-// var $firstName = "bhikam"
+// let firstName = "harshit";
+// console.log(firstName[0])
+// console.log(firstName[2])
+// console.log(firstName.length)
+
+// let firstName = "harshit ";
+// console.log(firstName.length);
+
+// let firstName = "harshit";
+// console.log(firstName[firstName.length]);
+
+// let firstName = "harshit";
+// console.log(firstName[firstName.length-1]);
+
+// let firstName = "    harshit    ";
+// firstName.trim();
+// console.log(firstName.length);
+
+// let firstName = "    harshit    ";
+// firstName = firstName.trim();
+// console.log(firstName.length);
+
+// let firstName = "harshit";
+// firstName = firstName.toUpperCase();
+// console.log(firstName)
+
+// let firstName = "harshit";
+// let newString = firstName.slice(0,4);
+// console.log(newString)
+
+// let firstName = "harshit";
+// let newString = firstName.slice(2,5);
+// console.log(newString)
+
+// let firstName = "harshit";
+// let newString = firstName.slice(1);
+// console.log(newString)
+
+// let firstName = "harshit";
+// console.log(typeof firstName)
+
+
+// How to convert number into string?
+// let age = 22;
+// age = age + "";
+// console.log(typeof(age))
+
+// let age = "22";
+// console.log(typeof(age));
+
+// How to convert string into Number
+// let age = "22";
+// age = +age
+// console.log(typeof(age))
+
+// let age = 22;
+// console.log(typeof(String(age)))
+
+// let age = "22";
+// console.log(typeof(Number(age)))
+
+// let string1 = "harshit";
+// let string2= "bhikam";
+// console.log(string1 + string2)
+
+// let string1 = "harshit";
+// let string2 = "bhikam";
+// console.log(string1 + "" + string2)
+
+// let string1 = "harshit";
+// let string2 = "bhikam";
+// console.log(string + "" + string2)
+
+// let string1 = "harshit";
+// let string2 = "bhikam";
+// console.log(string1 + "" + string2)
+
+// let string1 = "harshit";
+// let string2 = "bhikam";
+// console.log(string1 + " " + string2)
+
+// let string1 = "harshit";
+// let string2 = "bhikam";
+// console.log(string1 + "        " + string2)
+
+// let string1 = "harshit";
+// let string2 = 22;
+// console.log(string1 + string2)
+
+// let string1 = "harshit";
+// let string2 = 22;
+// let finalString = typeof(string1 + string2);
+// console.log(finalString)
+
+// let string1 = 22;
+// let string2 = "harshit";
+// let finalString = typeof(string1 + string2);
+// console.log(finalString)
+
+// let string1 = "harshit";
+// let string2 = 22;
+// let finalString = typeof(string1 * string2);
+// console.log(finalString)
+
+// let string1 = "20";
+// let string2 = "20";
+// console.log(+string1 + +string2);
+// console.log(typeof(+string1 + +string2))
+
+// let string1 = "harshit";
+// let string2 = 2;
+// now you have to print      my name is harshit and my age is 22    (harshit or 2 ki jagah variable ka use krna h)
+
+// let string1;
+// console.log(string1);
+// console.log(typeof(string1))
+
+// var string1;
+// console.log(string1);
+// console.log(typeof(string1))
+
+// const string1;
+// console.log(string1);
+// console.log(typeof(string1))
+// console.log(1+1)
+
+// let firstName;
+// console.log(firstName);
+// console.log(typeof(firstName));
+// firstName = "harshit";
+// console.log(firstName)
+// console.log(typeof(firstName))
+
+// var firstName;
+// console.log(firstName);
+// console.log(typeof(firstName));
+// firstName = "harshit";
+// console.log(firstName)
+// console.log(typeof(firstName))
+
+// const firstName;
+// console.log(firstName);
+// console.log(typeof(firstName));
+// firstName = "harshit";
+// console.log(firstName)
+// console.log(typeof(firstName))
+
+// console.log(typeof(null))
+
+// let myNumber = 123;
+// console.log(myNumber)
+
+// let myNumber = 123;
+// console.log(BigInt(myNumber));
+// console.log(typeof(BigInt(myNumber)));
+
+// let myNumber = 123n;
+// console.log(typeof(myNumber))
+
+
+
+
+
+
+
+
